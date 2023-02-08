07741fdff1d66e51d01dcc2f6602c1bd030f9d6d
Fako <oluwakunle@mrdraper.ae>
2023-02-08 18:54:29 +0400

    Calculation for average ratings

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -13,4 +13,10 @@ class Spot < ApplicationRecord
       images.create!(url: image_url)
     end
   end
+
+  def average_ratings
+    ratings = reviews.pluck(:rating)
+    total_rating = ratings.inject(0, :+)
+    (total_rating.to_f / ratings.size).ceil(1)
+  end
 end
--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -15,11 +15,25 @@ RSpec.describe Spot, type: :model do
   end
 
   describe "#add_images" do
-    it 'should list all images for a spot' do
+    it 'should add images for a spot' do
       spot = create(:spot)
       images = create_list(:image, 4).pluck(:url)
 
       expect { spot.add_images(images) }.to change { Image.count }.by(4)
+    end
+  end
+
+  describe "#average_ratings" do
+    it 'should list average ratings for a spot' do
+      spot = create(:spot)
+      create(:review, rating: 5, spot_id: spot.id)
+      create(:review, rating: 3, spot_id: spot.id)
+      create(:review, rating: 5, spot_id: spot.id)
+      create(:review, rating: 4, spot_id: spot.id)
+      create(:review, rating: 1, spot_id: spot.id)
+      create(:review, rating: 2, spot_id: spot.id)
+
+      expect(spot.average_ratings).to eq(3.4)
     end
   end
 end
